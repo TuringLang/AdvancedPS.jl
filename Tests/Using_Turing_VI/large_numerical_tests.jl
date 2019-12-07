74792495335f769887bab186b3b58b537cbb8138
donhausk <42518029+donhausk@users.noreply.github.com>
2019-12-07 15:49:21 +0000

    Bundle Tests

---
+++ Tests/Using_Turing_VI/large_numerical_tests.jl
@@ -0,0 +1,105 @@
+
+@testset "test_against_turing.jl" begin
+
+    @numerical_testset "apf inference" begin
+        N = 5000
+
+
+        ## Testset1
+        y = [0 for i in 1:10]
+
+        # We use the Turing model as baseline
+        chn_base = sample(large_demo(y),PG(20),N)
+
+        #############################################
+        # Using a Proposal                          #
+        #############################################
+        f = large_demo_apf_proposal
+        task = create_task(f,y)
+        model = PFModel(task)
+        tcontainer = VarInfo()
+
+        ##SMC
+        alg = AdvancedPS.SMCAlgorithm()
+        uf = AdvancedPS.SMCUtilityFunctions(deepcopy, set_retained_vns_del_by_spl!, empty!, tonamedtuple)
+        caps1 = sample(model, alg, uf, tcontainer, 5*N)
+        check_numerical(caps1,chn_base, 0.1, 0.2)
+
+        # PG
+        alg = AdvancedPS.PGAlgorithm(20)
+        uf = AdvancedPS.PGUtilityFunctions(deepcopy, set_retained_vns_del_by_spl!, empty!, tonamedtuple)
+        caps1 = sample(model, alg, uf, tcontainer, N)
+        check_numerical(caps1,chn_base,0.1, 0.2)
+
+
+
+        #############################################
+        # No Proposal                               #
+        #############################################
+        f = large_demo_apf
+        task = create_task(f,y)
+        model = PFModel(task)
+        tcontainer = VarInfo()
+
+        #SMC
+        alg = AdvancedPS.SMCAlgorithm()
+        uf = AdvancedPS.SMCUtilityFunctions(deepcopy, set_retained_vns_del_by_spl!, empty!, tonamedtuple)
+        caps1 = sample(model, alg, uf, tcontainer, 5*N)
+        check_numerical(caps1,chn_base, 0.1, 0.2)
+
+        # PG
+        alg = AdvancedPS.PGAlgorithm(20)
+        uf = AdvancedPS.PGUtilityFunctions(deepcopy, set_retained_vns_del_by_spl!, empty!, tonamedtuple)
+        caps1 = sample(model, alg, uf, tcontainer, N)
+        check_numerical(caps1,chn_base,0.1, 0.2)
+
+
+
+        ## Testset2
+        y = [-0.1*i for i in 1:10]
+
+        # We use the Turing model as baseline
+        chn_base = sample(large_demo(y),PG(20),N)
+
+        #############################################
+        # Using a Proposal                          #
+        #############################################
+        f = large_demo_apf_proposal
+        task = create_task(f,y)
+        model = PFModel(task)
+        tcontainer = VarInfo()
+
+        #SMC
+        alg = AdvancedPS.SMCAlgorithm()
+        uf = AdvancedPS.SMCUtilityFunctions(deepcopy, set_retained_vns_del_by_spl!, empty!, tonamedtuple)
+        caps1 = sample(model, alg, uf, tcontainer, 5*N)
+        check_numerical(caps1,chn_base,0.1, 0.2)
+
+        # PG
+        alg = AdvancedPS.PGAlgorithm(20)
+        uf = AdvancedPS.PGUtilityFunctions(deepcopy, set_retained_vns_del_by_spl!, empty!, tonamedtuple)
+        caps1 = sample(model, alg, uf, tcontainer, N)
+        check_numerical(caps1,chn_base, 0.1, 0.2)
+
+
+        #############################################
+        # No Proposal                               #
+        #############################################
+        f = large_demo_apf
+        task = create_task(f,y)
+        model = PFModel(task)
+        tcontainer = VarInfo()
+
+        #SMC
+        alg = AdvancedPS.SMCAlgorithm()
+        uf = AdvancedPS.SMCUtilityFunctions(deepcopy, set_retained_vns_del_by_spl!, empty!, tonamedtuple)
+        caps1 = sample(model, alg, uf, tcontainer, 5*N)
+        check_numerical(caps1,chn_base,0.21, 0.2)
+
+        # PG
+        alg = AdvancedPS.PGAlgorithm(20)
+        uf = AdvancedPS.PGUtilityFunctions(deepcopy, set_retained_vns_del_by_spl!, empty!, tonamedtuple)
+        caps1 = sample(model, alg, uf, tcontainer, N)
+        check_numerical(caps1,chn_base, 0.1, 0.2)
+    end
+end
