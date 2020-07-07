93d0ddb5c071ba928eb8c24b839d8d5fc384a4f4
JS Denain <denain.js@gmail.com>
2020-07-07 17:29:49 +0200

    remove function resample from tests

--- test/resampling.jl
+++ test/resampling.jl
@@ -8,11 +8,9 @@ using AdvancedPS
     resStratified = resample_stratified(D, num_samples )
     resMultinomial= resample_multinomial(D, num_samples )
     resResidual   = resample_residual(D, num_samples )
-    resample(D)
-    resSystematic2=resample(D, num_samples )
+
 
     @test sum(resSystematic .== 2) ≈ (num_samples * 0.4) atol=1e-3*num_samples
-    @test sum(resSystematic2 .== 2) ≈ (num_samples * 0.4) atol=1e-3*num_samples
     @test sum(resStratified .== 2) ≈ (num_samples * 0.4) atol=1e-3*num_samples
     @test sum(resMultinomial .== 2) ≈ (num_samples * 0.4) atol=1e-2*num_samples
     @test sum(resResidual .== 2) ≈ (num_samples * 0.4) atol=1e-2*num_samples
