455f17c05c1d83bff3a6a2aa8e0b2cf753fa02a0
JS Denain <denain.js@gmail.com>
2020-07-13 16:07:03 +0200

    add indentation and return to return statements in sweep.jl

--- src/sweep.jl
+++ src/sweep.jl
@@ -11,52 +11,52 @@ scheme,
 ref::Union{Particle, Nothing} = nothing;
 weights = getweights(pc)
 )
-# check that weights are not NaN
-@assert !any(isnan, weights)
+    # check that weights are not NaN
+    @assert !any(isnan, weights)
 
-# sample ancestor indices
-n = length(pc)
-nresamples = ref === nothing ? n : n - 1
-indx = scheme(weights, nresamples)
+    # sample ancestor indices
+    n = length(pc)
+    nresamples = ref === nothing ? n : n - 1
+    indx = scheme(weights, nresamples)
 
-# count number of children for each particle
-num_children = zeros(Int, n)
-@inbounds for i in indx
-    num_children[i] += 1
-end
+    # count number of children for each particle
+    num_children = zeros(Int, n)
+    @inbounds for i in indx
+        num_children[i] += 1
+    end
 
-# fork particles
-particles = collect(pc)
-children = similar(particles)
-j = 0
-@inbounds for i in 1:n
-    ni = num_children[i]
+    # fork particles
+    particles = collect(pc)
+    children = similar(particles)
+    j = 0
+    @inbounds for i in 1:n
+        ni = num_children[i]
 
-    if ni > 0
-        # fork first child
-        pi = particles[i]
-        isref = pi === ref
-        p = isref ? fork(pi, isref) : pi
-        children[j += 1] = p
+        if ni > 0
+            # fork first child
+            pi = particles[i]
+            isref = pi === ref
+            p = isref ? fork(pi, isref) : pi
+            children[j += 1] = p
 
-        # fork additional children
-        for _ in 2:ni
-            children[j += 1] = fork(p, isref)
+            # fork additional children
+            for _ in 2:ni
+                children[j += 1] = fork(p, isref)
+            end
         end
     end
-end
 
-if ref !== nothing
-    # Insert the retained particle. This is based on the replaying trick for efficiency
-    # reasons. If we implement PG using task copying, we need to store Nx * T particles!
-    @inbounds children[n] = ref
-end
+    if ref !== nothing
+        # Insert the retained particle. This is based on the replaying trick for efficiency
+        # reasons. If we implement PG using task copying, we need to store Nx * T particles!
+        @inbounds children[n] = ref
+    end
 
-# replace particles and log weights in the container with new particles and weights
-pc.vals = children
-reset_logweights!(pc)
+    # replace particles and log weights in the container with new particles and weights
+    pc.vals = children
+    reset_logweights!(pc)
 
-pc
+    return pc
 end
 
 
@@ -72,14 +72,14 @@ resampler::ResampleWithESSThreshold,
 ref::Union{Particle,Nothing} = nothing;
 weights = getweights(pc)
 )
-# Compute the effective sample size ``1 / ∑ wᵢ²`` with normalized weights ``wᵢ``
-ess = inv(sum(abs2, weights))
+    # Compute the effective sample size ``1 / ∑ wᵢ²`` with normalized weights ``wᵢ``
+    ess = inv(sum(abs2, weights))
 
-if ess ≤ resampler.threshold * length(pc)
-    resample_propagate!(pc, resampler.scheme, ref; weights = weights)
-end
+    if ess ≤ resampler.threshold * length(pc)
+        resample_propagate!(pc, resampler.scheme, ref; weights = weights)
+    end
 
-pc
+    return pc
 end
 
 
@@ -89,44 +89,44 @@ Check if the final time step is reached, and otherwise reweight the particles by
 considering the next observation.
 """
 function reweight!(pc::ParticleContainer)
-n = length(pc)
+    n = length(pc)
 
-particles = collect(pc)
-numdone = 0
-for i in 1:n
-    p = particles[i]
+    particles = collect(pc)
+    numdone = 0
+    for i in 1:n
+        p = particles[i]
 
-    # Obtain ``\\log p(yₜ | y₁, …, yₜ₋₁, x₁, …, xₜ, θ₁, …, θₜ)``, or `nothing` if the
-    # the execution of the model is finished.
-    # Here ``yᵢ`` are observations, ``xᵢ`` variables of the particle filter, and
-    # ``θᵢ`` are variables of other samplers.
-    score = Libtask.consume(p)
+        # Obtain ``\\log p(yₜ | y₁, …, yₜ₋₁, x₁, …, xₜ, θ₁, …, θₜ)``, or `nothing` if the
+        # the execution of the model is finished.
+        # Here ``yᵢ`` are observations, ``xᵢ`` variables of the particle filter, and
+        # ``θᵢ`` are variables of other samplers.
+        score = Libtask.consume(p)
 
-    if score === nothing
-        numdone += 1
-    else
-        # Increase the unnormalized logarithmic weights, accounting for the variables
-        # of other samplers.
-        increase_logweight!(pc, i, score + getlogp(p.vi))
+        if score === nothing
+            numdone += 1
+        else
+            # Increase the unnormalized logarithmic weights, accounting for the variables
+            # of other samplers.
+            increase_logweight!(pc, i, score + getlogp(p.vi))
 
-        # Reset the accumulator of the log probability in the model so that we can
-        # accumulate log probabilities of variables of other samplers until the next
-        # observation.
-        resetlogp!(p.vi)
+            # Reset the accumulator of the log probability in the model so that we can
+            # accumulate log probabilities of variables of other samplers until the next
+            # observation.
+            resetlogp!(p.vi)
+        end
     end
-end
 
-# Check if all particles are propagated to the final time point.
-numdone == n && return true
+    # Check if all particles are propagated to the final time point.
+    numdone == n && return true
 
-# The posterior for models with random number of observations is not well-defined.
-if numdone != 0
-    error("mis-aligned execution traces: # particles = ", n,
-          " # completed trajectories = ", numdone,
-          ". Please make sure the number of observations is NOT random.")
-end
+    # The posterior for models with random number of observations is not well-defined.
+    if numdone != 0
+        error("mis-aligned execution traces: # particles = ", n,
+            " # completed trajectories = ", numdone,
+            ". Please make sure the number of observations is NOT random.")
+    end
 
-return false
+    return false
 end
 
 """
@@ -138,45 +138,45 @@ Del Moral, P., Doucet, A., & Jasra, A. (2006). Sequential monte carlo samplers.
 Journal of the Royal Statistical Society: Series B (Statistical Methodology), 68(3), 411-436.
 """
 function sweep!(pc::ParticleContainer, resampler)
-# Initial step:
+    # Initial step:
 
-# Resample and propagate particles.
-resample_propagate!(pc, resampler)
-
-# Compute the current normalizing constant ``Z₀`` of the unnormalized logarithmic
-# weights.
-# Usually it is equal to the number of particles in the beginning but this
-# implementation covers also the unlikely case of a particle container that is
-# initialized with non-zero logarithmic weights.
-logZ0 = logZ(pc)
-
-# Reweight the particles by including the first observation ``y₁``.
-isdone = reweight!(pc)
-
-# Compute the normalizing constant ``Z₁`` after reweighting.
-logZ1 = logZ(pc)
-
-# Compute the estimate of the log evidence ``\\log p(y₁)``.
-logevidence = logZ1 - logZ0
-
-# For observations ``y₂, …, yₜ``:
-while !isdone
     # Resample and propagate particles.
     resample_propagate!(pc, resampler)
 
     # Compute the current normalizing constant ``Z₀`` of the unnormalized logarithmic
     # weights.
+    # Usually it is equal to the number of particles in the beginning but this
+    # implementation covers also the unlikely case of a particle container that is
+    # initialized with non-zero logarithmic weights.
     logZ0 = logZ(pc)
 
-    # Reweight the particles by including the next observation ``yₜ``.
+    # Reweight the particles by including the first observation ``y₁``.
     isdone = reweight!(pc)
 
     # Compute the normalizing constant ``Z₁`` after reweighting.
     logZ1 = logZ(pc)
 
-    # Compute the estimate of the log evidence ``\\log p(y₁, …, yₜ)``.
-    logevidence += logZ1 - logZ0
-end
+    # Compute the estimate of the log evidence ``\\log p(y₁)``.
+    logevidence = logZ1 - logZ0
 
-return logevidence
+    # For observations ``y₂, …, yₜ``:
+    while !isdone
+        # Resample and propagate particles.
+        resample_propagate!(pc, resampler)
+
+        # Compute the current normalizing constant ``Z₀`` of the unnormalized logarithmic
+        # weights.
+        logZ0 = logZ(pc)
+
+        # Reweight the particles by including the next observation ``yₜ``.
+        isdone = reweight!(pc)
+
+        # Compute the normalizing constant ``Z₁`` after reweighting.
+        logZ1 = logZ(pc)
+
+        # Compute the estimate of the log evidence ``\\log p(y₁, …, yₜ)``.
+        logevidence += logZ1 - logZ0
+    end
+
+    return logevidence
 end
