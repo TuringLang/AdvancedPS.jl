4cd74b0996894604af7fcc1674b3317eba41572e
FredericWantiez <frederic.wantiez@gmail.com>
2021-09-04 14:58:32 +0100

    Fix naming

--- src/rng.jl
+++ src/rng.jl
@@ -4,6 +4,7 @@ using Distributions
 
 import Base.rand
 import Random.seed!
+import Random123: set_counter!
 
 # Default RNG type for when nothing is specified
 _BASE_RNG = Philox2x
@@ -29,7 +30,7 @@ end
 Initialize TracedRNG with r as the inner RNG 
 """
 function TracedRNG(r::Random123.AbstractR123)
-    set_counter!(r, 1)
+    set_counter!(r, 0)
     return TracedRNG(1, r, typeof(r.key)[])
 end
 
@@ -48,32 +49,23 @@ Random.rng_native_52(rng::TracedRNG{U}) where {U} = Random.rng_native_52(rng.rng
 Base.rand(rng::TracedRNG{U}, ::Type{T}) where {U,T} = Base.rand(rng.rng, T)
 
 """
-    split(r::TracedRNG, n::Integer)
+    split(key::Integer, n::Integer)
 
-Split inner RNG into n new TracedRNG
+Split key into n new keys
 """
-function split(r::TracedRNG{T}, n::Integer) where {T}
-    return map(i -> hash(r.rng.key, convert(UInt, r.rng.ctr1 + i)), 1:n)
+function split(key::Integer, n::Integer=1) where {T}
+    return map(i -> hash(key, convert(UInt, i)), 1:n)
 end
 
 """
-    seed!(r::TracedRNG, seed::Number)
-
-Set the key of the inner RNG as `seed`
-"""
-function seed!(r::TracedRNG{T}, seed) where {T}
-    return seed!(r.rng, seed)
-end
-
-"""
-    load_state(r::TracedRNG, seed)
+    load_state!(r::TracedRNG, seed)
 
 Load state from current model iteration. Random streams are now replayed
 """
-function load_state(rng::TracedRNG{T}) where {T}
+function load_state!(rng::TracedRNG{T}) where {T}
     key = rng.keys[rng.count]
     Random.seed!(rng.rng, key)
-    return set_counter!(rng.rng, rng.count)
+    return set_counter!(rng.rng, 0)
 end
 
 """
@@ -81,9 +73,9 @@ end
 
 Set key and counter of inner RNG to key and the running model step
 """
-function update_rng!(rng::TracedRNG{T}, key) where {T}
-    seed!(rng, key)
-    return set_counter!(rng.rng, rng.count)
+function seed!(rng::TracedRNG{T}, key) where {T}
+    seed!(rng.rng, key)
+    return set_counter!(rng.rng, 0)
 end
 
 """ 
@@ -102,7 +94,11 @@ Base.copy(r::TracedRNG{T}) where {T} = TracedRNG(r.count, copy(r.rng), copy(r.ke
 
 Set the counter of the TracedRNG, used to keep track of the current model step
 """
-set_count!(r::TracedRNG, n::Integer) = r.count = n
+set_counter!(r::TracedRNG, n::Integer) = r.count = n
 
-inc_count!(r::TracedRNG, n::Integer) = r.count += n
-inc_count!(r::TracedRNG) = inc_count!(r, 1)
+"""
+    inc_counter!(r::TracedRNG, n::Integer=1)
+
+Increase the model step counter by n
+"""
+inc_counter!(r::TracedRNG, n::Integer=1) = r.count += n
