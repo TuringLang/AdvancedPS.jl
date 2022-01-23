3661d5583c20bc66edda5ba5fdccafb152a5ac06
Hong Ge <hg344@cam.ac.uk>
2022-01-23 19:37:04 +0000

    Update model.jl

--- src/model.jl
+++ src/model.jl
@@ -6,10 +6,3 @@ Observe sample `x` from distribution `dist` and yield its log-likelihood value.
 function observe(dist::Distributions.Distribution, x)
     return Libtask.produce(Distributions.loglikelihood(dist, x))
 end
-
-function (instr::Libtask.Instruction{typeof(observe)})()
-    dist = Libtask.val(instr.input[1])
-    x = Libtask.val(instr.input[2])
-    result = Distributions.loglikelihood(dist, x)
-    return Libtask.internal_produce(instr, result)
-end
